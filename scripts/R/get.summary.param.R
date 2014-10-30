7bc192e216a2f720754864bdb804ac905af6ffeb
heejungshim <hjshim@gmail.com>
2014-10-30 09:48:51 -0500

    add changes to hanlde multiple parameters

--- scripts/R/get.summary.param.R
+++ scripts/R/get.summary.param.R
@@ -7,11 +7,12 @@
 ## input.file : filename that contains MCMC samples.
 ## output.file : filename where output be written.
 ## thresh.CI : this script will compute thresh.CI% CI.
+## numParas : number of parameters in input.file
 ## [Output]
-## output file contains four numbers - sample mean, median, thresh.CI% CI
+## output file contains numParam number of rows, and each row contains four numbers - each column sample mean, median, thresh.CI% CI
 ##
 ## example:
-## Rscript get.summary.param.R RESmLambda RESmLambda.sum 95
+## Rscript get.summary.param.R RESmLambda RESmLambda.sum 95 numParam
 ## 
 ## Copyright (C) 2014 Heejung Shim
 ##
@@ -24,16 +25,28 @@ print(args)
 input.file = args[1]
 output.file = args[2]
 thresh.CI = as.numeric(args[3])
+numParam = as.numeric(args[4])
 
-MCMCsamples = scan(input.file, what = double())
-mean.out = mean(MCMCsamples)
+
 lower.bound = (100-thresh.CI)/2/100
 upper.bound = 1- (100-thresh.CI)/2/100
-quantile.sum = quantile(MCMCsamples, probs=c(lower.bound, 0.5, upper.bound))
-out = c(mean.out, as.numeric(quantile.sum[2]), as.numeric(quantile.sum[1]), as.numeric(quantile.sum[3]))
 
-cat(out, file = output.file)
 
+if(numParam == 1){
+  MCMCsamples = scan(input.file, what = double())
+  mean.out = mean(MCMCsamples)
+  quantile.sum = quantile(MCMCsamples, probs=c(lower.bound, 0.5, upper.bound))
+  out = c(mean.out, as.numeric(quantile.sum[2]), as.numeric(quantile.sum[1]), as.numeric(quantile.sum[3]))
+  cat(out, file = output.file)
+}else{
+  MCMCsamples = read.table(input.file)
+  mean.out = apply(MCMCsamples, 2, mean)
+  quantile.sum = apply(MCMCsamples, 2, quantile, probs = c(lower.bound, 0.5, upper.bound))
+  out = matrix(data=NA, nc = 4, nr = numParam)
+  out[,1] = t(mean.out)
+  out[,2:4] = t(quantile.sum)
+  write.table(out, file=output.file,quote=FALSE,row.names = FALSE, col.names = FALSE)
+}
 
 
 
